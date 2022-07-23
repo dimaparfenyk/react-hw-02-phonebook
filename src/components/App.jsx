5f7209f82ccd3a7b454219fbb63ec4015d9ef152
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2022-07-23 17:12:05 +0300

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,24 @@ import { Container, Box } from "./App.styled";
 
 export class App extends Component {
   state = {
-    contacts: [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
+    contacts: [],
     filter: ''
   };
 
+  componentDidMount() { 
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+   
+    if (parsedContacts) {
+      this.setState({contacts:parsedContacts})
+    }
+  };
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+   }
+  
+   };
   addContact = contact => {
     const { contacts } = this.state
   
